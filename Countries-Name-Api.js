8b122bae0766f0a999d0884a7f9c72605176c5b0
Aoyon123 <49103044+Aoyon123@users.noreply.github.com>
2021-03-06 20:03:21 +0600

    Country Details

--- Countries-Name-Api.js
+++ Countries-Name-Api.js
@@ -4,18 +4,76 @@ fetch('https://restcountries.eu/rest/v2/all')
 
 const displayCountries=countries=>{
 const countriesDiv=document.getElementById('countries');
- for(let i=0;i<countries.length;i++)
-  {
-     const country=countries[i];
-     const countryDiv=document.createElement('div')
-     const h3=document.createElement('h3')
-     h3.innerText=country.name;
-     const p=document.createElement('p')
-     p.innerText=country.capital;
-     countryDiv.appendChild(h3);
-     countryDiv.appendChild(p);
 
-     countriesDiv.appendChild(countryDiv);
-  }
+
+countries.forEach(country=> {
+
+    //const country=countries[i];
+    const countryDiv=document.createElement('div');
+    countryDiv.className=`country`;
+    const h3=document.createElement('h3')
+    h3.innerText=country.name;     
+    const p=document.createElement('p')
+    p.innerText=country.capital;
+    countryDiv.appendChild(h3);
+    countryDiv.appendChild(p);
+    
+     const countryInfo=`
+    <h3 class="country-name">${country.name}</h3>
+     <p>${country.capital}</p>
+
+     <button onclick="displayCountryDetail('${country.name}')">Details</button>
+     `;
+     countryDiv.innerHTML=countryInfo;
+
+
+    countriesDiv.appendChild(countryDiv);
+  
+    
+});
 
 }
+
+const displayCountryDetail=name=>{
+        const url=`https://restcountries.eu/rest/v2/name/${name}`
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>renderCountryInfo(data[0]));
+}
+
+const renderCountryInfo= country=>{
+    const countryDiv=document.getElementById("countryDetail")
+    countryDiv.innerHTML=`
+    <h1>${country.name}<h1>
+    <p>Population :${country.population}</p>
+    <p> Area :${country.area}</p>
+    <img src="${country.flag}">
+    
+    
+    `
+}
+
+
+//  for(let i=0;i<countries.length;i++)
+//   {
+//      const country=countries[i];
+//      const countryDiv=document.createElement('div');
+//      countryDiv.className=`country`;
+//     //  const h3=document.createElement('h3')
+//     //  h3.innerText=country.name;
+//     //  const p=document.createElement('p')
+//     //  p.innerText=country.capital;
+//     //  countryDiv.appendChild(h3);
+//     //  countryDiv.appendChild(p);
+    
+//     const countryInfo=`
+//     <h3 class="country-name">${country.name}</h3>
+//     <p>${country.capital}</p>
+//     `
+//     countryDiv.innerHTML=countryInfo;
+
+
+//      countriesDiv.appendChild(countryDiv);
+  //}
+
+
